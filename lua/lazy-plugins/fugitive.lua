4ac936e3fff5483bd01d11693aed2e45366b173c
bp64 <scratchpeterson@gmail.com>
2024-03-03 02:32:30 -0600

    moved default plugins into the plugins directory

---
+++ lua/lazy-plugins/fugitive.lua
@@ -0,0 +1,4 @@
+return {
+  'tpope/vim-fugitive', -- Git commands in nvim
+}
+-- vim: ts=2 sts=2 sw=2 et
---
+++ lua/lazy-plugins/indent-blanklines.lua
@@ -0,0 +1,7 @@
+-- Indentions config
+return { -- Add indentation guides even on blank lines
+  'lukas-reineke/indent-blankline.nvim',
+  main = 'ibl',
+  opts = {},
+}
+-- vim: ts=2 sts=2 sw=2 et
