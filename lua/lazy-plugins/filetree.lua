1d047e2b784c96ee178dfc7bd0b0cdd24bbc4cfe
bp64 <scratchpeterson@gmail.com>
2024-03-03 02:02:55 -0600

    config neotree and add keymap

--- lua/lazy-plugins/filetree.lua
+++ lua/lazy-plugins/filetree.lua
@@ -10,7 +10,38 @@ return {
     'MunifTanjim/nui.nvim',
   },
   config = function()
-    require('neo-tree').setup {}
+    require('neo-tree').setup {
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          never_show = {
+            '.DS_Store',
+          },
+        },
+      },
+    }
+
+    vim.keymap.set('n', '\\', function()
+      local reveal_file = vim.fn.expand '%:p'
+      if reveal_file == '' then
+        reveal_file = vim.fn.getcwd()
+      else
+        local f = io.open(reveal_file, 'r')
+        if f then
+          f.close(f)
+        else
+          reveal_file = vim.fn.getcwd()
+        end
+      end
+      require('neo-tree.command').execute {
+        action = 'focus', -- OPTIONAL, this is the default value
+        source = 'filesystem', -- OPTIONAL, this is the default value
+        position = 'left', -- OPTIONAL, this is the default value
+        reveal_file = reveal_file, -- path to file or folder to reveal
+        reveal_force_cwd = true, -- change cwd without asking if needed
+        toggle = true,
+      }
+    end, { desc = 'Open neo-tree at current file or working directory' })
   end,
 }
 -- vim: ts=2 sts=2 sw=2 et
